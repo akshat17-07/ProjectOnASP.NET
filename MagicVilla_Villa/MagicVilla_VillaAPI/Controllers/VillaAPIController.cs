7e6f927140b7a400fe0f67f118f247716fc4ec9f
Akshat Harlalka <avh5936@psu.edu>
2023-05-28 03:42:40 -0400

    added states code

--- MagicVilla_Villa/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_Villa/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,9 +11,31 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult <IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
+
+        }
+
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(VillaDTO))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult <VillaDTO> GetVilla(int id)
+        {
+            if (id == 0) {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+            if (villa == null) {
+                return NotFound();
+            }
+
+            return Ok(villa);
 
         }
     }
